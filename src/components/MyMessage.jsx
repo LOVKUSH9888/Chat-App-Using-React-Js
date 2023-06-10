f221844858cbf4acf3fe3a34f0fd77871684b1db
LOVKUSH <lovkushyadav9888@gmail.com>
2023-06-10 17:19:58 +0530

    coding the msg component

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+// This component renders a message based on the provided 'message' prop
 
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+  if (message.attachments && message.attachments.length > 0) {
+    // If the message has attachments, render an image
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: 'right' }}
+      />
+    );
+  }
+
+  // If the message does not have attachments, render a text message
   return (
-    <div>
-      
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+      {message.text}
     </div>
-  )
-}
+  );
+};
 
-export default MyMessage
+export default MyMessage;
